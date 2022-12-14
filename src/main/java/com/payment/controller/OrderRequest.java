7b77ba66e246975383bb9863cd75cdc16f13633b
Shantanu-Navghane <shantanu.navghane@capgemini.com>
2022-12-14 22:27:45 +0530

    1st

---
+++ src/main/java/com/payment/controller/OrderRequest.java
@@ -0,0 +1,51 @@
+package com.payment.controller;
+
+import java.math.BigInteger;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="pay")
+public class OrderRequest {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private String customerName;
+	private String email;
+	private String phoneNumber;
+	private BigInteger amount;
+	
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public BigInteger getAmount() {
+		return amount;
+	}
+	public void setAmount(BigInteger amount) {
+		this.amount = amount;
+	} 
+	
+	
+	
+
+}
